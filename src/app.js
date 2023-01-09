1da2fe7755c428f0e7c4af7babdae5e33578743b
edtneves <edianeunb@gmail.com>
2023-01-09 15:57:55 -0300

    carregando os 10 ultimo tweets

--- src/app.js
+++ src/app.js
@@ -6,146 +6,46 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const appUsers = []
-const usuarios = [];
-const tweet  = [];
+const usuarios = [
+  {
+    username: "bobesponja",
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+  }
+];
 
-  app.get("/usuarios", (request, response) => {
-    response.send(usuarios)
-  })
-  app.get("/tweet", (request, response) => {
-    response.send(tweet)
-  })
+const tweets = [];
+app.get("/usuarios", (_request, response) => {
+  response.send(usuarios)
+})
 
-  const signUp = [
-    {
-      username: "bobesponja",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-    } 
-  ];
+app.post('/sign-up', (req, res) => {
+  const newSignUp = req.body
+  usuarios.push(newSignUp)
+  res.status(201).send('OK');
+})
 
-  app.post('/sign-up', (req, res) => {
-    const newSignUp = req.body
-    const user = req.body
-    appUsers.push(user)
-    signUp.push(newSignUp)
-    res.status(201).send('OK');
-  })
-  app.get('/sign-up', (req, res) => {
-    res.send(signUp)
-  })
-
-  const tweets = [
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },{
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lulu",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "rbrbt",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "brb",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "frdt",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "lpfov",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "le",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-    {
-      username: "l",
-      avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "esalve"
-    },
-  ];
-  
-  app.post('/tweets', (req, res) => {
-    const newTweets = req.body
+app.post('/tweets', (req, res) => {
+  const newTweets = req.body
+  const userRegistered = usuarios.find(item => item.username === newTweets.username)
+  if (!userRegistered) {
+    return res.status(401).send('UNAUTHORIZED');
+  } else {
     tweets.push(newTweets)
-    if (tweets.username != appUsers.username) {
-      return res.status(401).send('UNAUTHORIZED');
-    } 
     res.status(201).send('OK');
-  })
+  }
+})
 
-  app.get('/tweets', (req, res) => {
-    const { limite } = req.query
-    //[...tweets].reverse()
-   // slice(-10, tweets.length)
-    const tweetsReverse = { ...tweets }
-    const ultimosTweets = tweetsReverse.reverse().slice(0, parseInt(limite))
-    res.send(ultimosTweets)
-  })
-
+app.get('/tweets', (_req, res) => {
+  const lastTweets = [];
+  const tweetsReverse = [...tweets]
+  const ultimosTweets = tweetsReverse.reverse().slice(0, 10)
+  for (let i = 0; i < ultimosTweets.length; i++) {
+    const userIndex = usuarios.findIndex(user => user.username === ultimosTweets[i].username)
+    lastTweets.push({ ...ultimosTweets[i], avatar: usuarios[userIndex].avatar })
+  }
+  res.status(200).send(lastTweets)
+})
 
 app.listen(PORT, () => {
-  console.log('funcionou')
+  console.log('Servidor rodando na porta:', PORT)
 })
